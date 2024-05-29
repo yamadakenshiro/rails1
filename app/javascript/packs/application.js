685c634d792d7267725e731bfc2f5ade653d7f10
山田謙士郎 <yamadakenshirou@yamadakenshirounoMacBook-Air.local>
2024-05-29 13:12:03 +0900

    kadai

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,11 @@
+//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+//= require activestorage
+
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -11,3 +19,16 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+var btn = document.getElementById('btn');
+var modal = document.getElementById('modal');
+
+btn.addEventListener('click', function() {
+  modal.style.display = 'block';
+})
+
+var closeBtn = document.getElementById('closeBtn');
+
+closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+})
